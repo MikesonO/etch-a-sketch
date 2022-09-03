2d8aa9ada91bf7713a7f01f6624ff6df631e8d97
mikeson <mikeson.jnr@gmail.com>
2022-09-03 20:55:45 +0100

    Add comments and organised code structure

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
+//Variables
 const canvas = document.querySelector("#canvas");
+const pixels = document.querySelector("div#canvas");
+let gridSize = document.querySelector("#gridSize");
+let reset = document.querySelector("#reset");
+let gridInfo = document.querySelector(".gridInfo");
 
 
-createGrid = () => {  //Default is 16x16
-  for (let i = 0; i < 256; i++) { //Creates 256 pixels
+createGrid = () => {
+  //Default is 16x16 = 256
+  for (let i = 0; i < 256; i++) {
     const pixels = document.createElement("div");
     pixels.classList.add("pixels");
     canvas.appendChild(pixels);
   }
 };
 
-let gridSize = document.querySelector("#gridSize");
-
-
+//Changes size of grid
 changeGrid = () => {
   canvas.innerHTML = "";
-
-  canvas.style.setProperty("grid-template-rows",`repeat(${gridSize.value}, 2fr)`);
-  canvas.style.setProperty("grid-template-columns",`repeat(${gridSize.value}, 2fr)`);
-
-for (let i = 0; i < gridSize.value * gridSize.value; i++) {
-  const pixels = document.createElement("div");
-  pixels.classList.add("pixels");
-  canvas.appendChild(pixels);
-}
+  canvas.style.setProperty("grid-template-rows", `repeat(${gridSize.value}, 2fr)`);
+  canvas.style.setProperty("grid-template-columns", `repeat(${gridSize.value}, 2fr)`);
+  for (let i = 0; i < gridSize.value * gridSize.value; i++) {
+    const pixels = document.createElement("div");
+    pixels.classList.add("pixels");
+    canvas.appendChild(pixels);
+  }
 };
 
-const pixels = document.querySelector("div#canvas"); //Draws on canvas
-pixels.addEventListener("mouseover", (event) =>{
-  event.target.classList.replace("pixels", "color");
-});
-
-
-
-let reset = document.querySelector("#reset");
-let gridInfo = document.querySelector(".gridInfo");
-reset.addEventListener("click", () => {
-  canvas.innerHTML="";
-  canvas.style.setProperty("grid-template-rows","repeat(16, 2fr)");
-  canvas.style.setProperty("grid-template-columns","repeat(16, 2fr)");
-  gridInfo.textContent = "16x16";
-  gridSize.value = 16;
-  createGrid();
-})
-
-
+//Specifies Grid Size based on range input
 gridSize.addEventListener("change", () => {
   gridInfo.textContent = `${gridSize.value}x${gridSize.value}`
   changeGrid();
 });
 
+//Draws on canvas
+pixels.addEventListener("mouseover", (event) => {
+  event.target.classList.replace("pixels", "color");
+});
+
+
+//Reset function - goes back to default settings
+reset.addEventListener("click", () => {
+  canvas.innerHTML = "";
+  canvas.style.setProperty("grid-template-rows", "repeat(16, 2fr)");
+  canvas.style.setProperty("grid-template-columns", "repeat(16, 2fr)");
+  gridInfo.textContent = "16x16";
+  gridSize.value = 16;
+  createGrid();
+})
 
 
 
