aa0fc2f987c7d09a0415bbb7cb1cb831314cfa97
mikeson <mikeson.jnr@gmail.com>
2022-09-03 19:30:24 +0100

    Add ability to change grid dimensions with slider

--- index.js
+++ index.js
@@ -3,10 +3,26 @@ const canvas = document.querySelector("#canvas");
 
 createGrid = () => {  //Default is 16x16
   for (let i = 0; i < 256; i++) { //Creates 256 pixels
-    const pixels= document.createElement("div");
+    const pixels = document.createElement("div");
     pixels.classList.add("pixels");
     canvas.appendChild(pixels);
   }
+};
+
+let gridSize = document.querySelector("#gridSize");
+
+
+changeGrid = () => {
+  canvas.innerHTML = "";
+
+  canvas.style.setProperty("grid-template-columns",`repeat(${gridSize.value}, 2fr)`);
+  canvas.style.setProperty("grid-template-rows",`repeat(${gridSize.value}, 2fr)`);
+
+for (let i = 0; i < gridSize.value * gridSize.value; i++) {
+  const pixels = document.createElement("div");
+  pixels.classList.add("pixels");
+  canvas.appendChild(pixels);
+}
 };
 
 const pixels = document.querySelector("div#canvas"); //Draws on canvas
@@ -21,6 +37,14 @@ reset.addEventListener("click", () => {
 })
 
 
+gridSize.addEventListener("change", () => {
+  let gridInfo = document.querySelector(".gridInfo");
+  gridInfo.textContent = `${gridSize.value}x${gridSize.value}`
+  console.log(gridSize.value);
+  changeGrid();
+});
+
+
+
 
 createGrid();
-
